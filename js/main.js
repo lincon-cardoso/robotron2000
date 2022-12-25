2fa6d19a5c0d5b0394ee13a385d30997591ac26c
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2022-12-25 15:44:10 -0300

    simplicifado

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-//Braço
-// const subtrai__pontos__braco = document.querySelector('.subtrai__pontos__braco');
-// const pontos__braço = document.querySelector('.pontos__braço');
-// const adiciona__pontos__braco = document.querySelector('.adiciona__pontos__braco');
-
-
 const controle = document.querySelectorAll(".controle-ajuste");
 
 controle.forEach((elemento)=> {
@@ -16,29 +10,15 @@ function maniculaDados(operacao,controle){
     const peca = controle.querySelector(".controle-contador");
 
     if(operacao ==="-"){
+       if(peca.value>0){
         peca.value = parseInt(peca.value)-1;
+       }
         
     }else {
-        peca.value = parseInt(peca.value)+1;
+        if(peca.value<99){
+            peca.value = parseInt(peca.value)+1;
+        }
     }
 
 }
 
-
-
-
-// function somanumero() {
-//     let novoValor = Number(pontos__braço.value);
-//     if (novoValor < 50) {
-//         pontos__braço.value = novoValor + 1;
-//       }
-    
-// }
-
-// function subtrai() {
-//     let novoValor = Number(pontos__braço.value);
-//     if (novoValor > 0) {
-//         pontos__braço.value = novoValor - 1;
-
-//     }
-// }
